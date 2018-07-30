20b6cb13f3565e02ed86238b4428684a980e7bb6
yueshangmx <654704420@qq.com>
2018-07-30 17:18:09 +0800

    修改逻辑

--- data/connect.php
+++ data/connect.php
@@ -11,10 +11,6 @@ header( 'Access-Control-Allow-Credentials:true' );
 header( "Access-Control-Allow-Origin:*" );
 header( 'Access-Control-Allow-Headers:x-requested-with,content-type' );
 
-$PHP_SELF=$_SERVER['PHP_SELF'];
-
-$dir='http://'.$_SERVER['HTTP_HOST'].substr($PHP_SELF,0,strrpos($PHP_SELF,'/')+1);
-
 $con = mysqli_connect( '127.0.0.1', 'vipcool', 'yhy214', 'db_crm', '3306' ) or die( '数据库连接异常!<br>' );
 
 $con->query( "SET NAMES utf8" );
